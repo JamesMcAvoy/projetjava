684f70f87fc130d4a77695fcc46f5c9955978b9d
cisco <mathieu.brisset@laposte.net>
2017-06-22 23:25:08 +0200

    Delete INotMoving.java

--- model/src/main/java/Elements/NotMoving/INotMoving.java
+++
@@ -1,9 +0,0 @@
-package Elements.NotMoving;
-
-import Factory.Location;
-
-public interface INotMoving {
-	public boolean breakable();
-	public Location getlocation();
-	public void setlocation(Location location);
-}
